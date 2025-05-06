8de26a762be0dc78ee2c4a62bf97dbf0f4e7356a
Calel <borgescalel@gmail.com>
2025-05-06 19:09:09 -0300

    feat: Adicionado schema e teste de schema de venda e categoria

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,15 +1,27 @@
-from schemas.vendas import VendasSchema
+from src.schemas import Vendas, Categoria
+import pytest
+from datetime import datetime
+from pydantic import ValidationError
+
 
 def test_vendas_dados_validos():
     from datetime import datetime
     
 
     # Criando um exemplo de dados válidos para o teste    
-dados_validos = {
-    "email": "comprador@exemplo.com",
-    "data": datetime.now(),
-    "valor": 100.50,
-    "produto": "Produto Exemplo",
-    "quantidade": 2,
-    "categoria": "Categoria Exemplo",
-}
+    dados_validos = {
+        "email": "comprador@exemplo.com",
+        "data": datetime.now(),
+        "valor": 100.50,
+        "produto": "Produto Exemplo",
+        "quantidade": 2,
+        "categoria": "Categoria 1",
+    }
+    venda = Vendas(**dados_validos)
+
+    assert venda.email == dados_validos["email"]
+    assert venda.data == dados_validos["data"]
+    assert venda.valor == dados_validos["valor"]
+    assert venda.produto == dados_validos["produto"]
+    assert venda.quantidade == dados_validos["quantidade"]
+    assert venda.categoria == dados_validos["categoria"]
