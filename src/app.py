4d29a6f73ef0869df8265f7864c918e7aae35710
Calel <borgescalel@gmail.com>
2025-05-19 23:37:36 -0300

    feat: adicionado log no sentry, na nuvem

--- src/app.py
+++ src/app.py
@@ -1,6 +1,16 @@
 from frontend import ExcelValidadorUI
 from backend import process_excel, excel_to_sql
-from logging import error, info
+import logging
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://7efef447db2aa2b67c5234001a0ae52f@o4509353079865344.ingest.us.sentry.io/4509353098280960",
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+    traces_sample_rate=1.0,
+    profiles_sample_rate=1.0
+)
 
 def main():
     # Initialize the UI
@@ -21,10 +31,12 @@ def main():
 
         if error:
             ui.display_wrong_message()
+            logging.error("Validation failed. Please correct the Excel file.")
+            sentry_sdk.capture_message("Validation failed :(. Please correct the Excel file")
         elif ui.display_save_message():
             excel_to_sql(df)
             ui.display_success_message()
-            info("File uploaded and saved to the database successfully.")
+            logging.info("File uploaded and saved to the database successfully.")
 
 if __name__ == "__main__":
     main()
